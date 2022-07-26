349c039922316b0772f7f8370e4cf8063db1a093
Felipe Barreto <felipbarreto@icloud.com>
2022-07-26 09:18:05 -0300

    Adicionado hook ao projeto

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,9 +1,14 @@
 /// <reference types="cypress" />
 
 context('Funcionalidade Login', () =>{
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    afterEach(() => {
+        cy.screenshot()
+    });
 
     it('Deve fazer login com sucesso', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click ()
@@ -12,7 +17,6 @@ context('Funcionalidade Login', () =>{
     })
 
     it('Deve exibir uma mensagem de erro ao inserir usuario inválidos', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('ste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click ()
@@ -22,7 +26,6 @@ context('Funcionalidade Login', () =>{
     })
 
     it('Deve exibir uma mensagem de erro ao inserir usuario inválidos', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('este@teste.com')
         cy.get('.woocommerce-form > .button').click ()
